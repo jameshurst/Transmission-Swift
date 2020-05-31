0347028cd76dc56778dee7103aec062b920edb26
James Hurst <james@jameshurst.ca>
2020-05-31 14:24:12 -0400

    Add mint bootstrap to run_ci.sh

--- scripts/run_ci.sh
+++ scripts/run_ci.sh
@@ -7,6 +7,8 @@ if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then
   exit -1
 fi
 
+tools/mint bootstrap --verbose
+
 scripts/format.sh
 if [ -n "$(git ls-files --others --modified --exclude-standard)" ]; then
   printf "\e[1;31mError: Found changes after running 'scripts/format.sh'.\e[0m\n"
